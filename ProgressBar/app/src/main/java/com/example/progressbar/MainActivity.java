ee1c6fe7c6dbb20829d161ced799a562af267b46
Hwangtaehun <r283@naver.com>
2025-12-27 19:29:44 +0900

    작업완료

--- ProgressBar/app/src/main/java/com/example/progressbar/MainActivity.java
+++ ProgressBar/app/src/main/java/com/example/progressbar/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.progressbar;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -25,5 +26,50 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        ButtonClickListener1 buttonClickListener1 = new ButtonClickListener1();
+        activityMainBinding.button.setOnClickListener(buttonClickListener1);
+
+        ButtonClickListnere2 buttonClickListnere2 = new ButtonClickListnere2();
+        activityMainBinding.button2.setOnClickListener(buttonClickListnere2);
+
+        ButtonClickListener3 buttonClickListener3 = new ButtonClickListener3();
+        activityMainBinding.button3.setOnClickListener(buttonClickListener3);
+        
+        ButtonClickListener4 buttonClickListener4 = new ButtonClickListener4();
+        activityMainBinding.button4.setOnClickListener(buttonClickListener4);
+    }
+
+    class ButtonClickListener1 implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            // 현재 값을 가져온다.
+            int value = activityMainBinding.progressBar4.getProgress();
+            activityMainBinding.textView.setText("value : " + value);
+        }
+    }
+
+    class ButtonClickListnere2 implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            // 현재 값을 설정한다.
+            activityMainBinding.progressBar4.setProgress(140);
+        }
+    }
+
+    class ButtonClickListener3 implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            // 10 만큼 증가한다.
+            activityMainBinding.progressBar4.incrementProgressBy(10);
+        }
+    }
+
+    class ButtonClickListener4 implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            // 10 만큼 감소한다.
+            activityMainBinding.progressBar4.incrementProgressBy(-10);
+        }
     }
 }
