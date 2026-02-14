c360b143345fab8a45acddb1030963f84df5b8fa
Hwangtaehun <r283@naver.com>
2026-02-14 21:46:51 +0900

    시스템메시지 완성

---
+++ SystemMessage/app/src/main/java/com/example/systemmessage/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.systemmessage;
+
+import android.Manifest;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.systemmessage.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    String [] permissionList = {
+            Manifest.permission.RECEIVE_BOOT_COMPLETED,
+            Manifest.permission.READ_PHONE_STATE,
+            Manifest.permission.RECEIVE_SMS
+    };
+    ActivityMainBinding activityMainBinding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        EdgeToEdge.enable(this);
+        setContentView(activityMainBinding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        requestPermissions(permissionList, 0);
+    }
+}
