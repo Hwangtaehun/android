ec58e510b29189f6ef4880f95a2759672dd261ca
Hwangtaehun <r283@naver.com>
2026-02-22 14:55:44 +0900

    메모 추가 기능 구현

--- App1Memo/app/build.gradle.kts
+++ App1Memo/app/build.gradle.kts
@@ -41,6 +41,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.protolite.well.known.types)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- App1Memo/app/src/main/java/com/example/app1memo/MemoAddActivity.java
+++ App1Memo/app/src/main/java/com/example/app1memo/MemoAddActivity.java
@@ -1,7 +1,12 @@
 package com.example.app1memo;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.inputmethod.InputMethodManager;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -12,6 +17,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.app1memo.databinding.ActivityMemoAddBinding;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class MemoAddActivity extends AppCompatActivity {
    ActivityMemoAddBinding activityMemoAddBinding;
@@ -37,6 +46,46 @@ public class MemoAddActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setHomeButtonEnabled(true);
         actionBar.setDisplayHomeAsUpEnabled(true);
+
+        // 제목에 포커스를 준다.
+        //activityMemoAddBinding.addMenoSubject.requestFocus();
+
+        // 키보드를 올려준다.
+        //InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+        //inputMethodManager.showSoftInput(activityMemoAddBinding.addMenoSubject, InputMethodManager.SHOW_FORCED);
+
+        // 키보드 쓰레드 기동
+        KeyboardThread keyboardThread = new KeyboardThread();
+        keyboardThread.start();
+    }
+
+    // 키보드 자동으로 보여주기 위한 쓰레드
+    class KeyboardThread extends Thread {
+        @Override
+        public void run() {
+            super.run();
+
+            SystemClock.sleep(500);
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    // 제목에 포커스를 준다.
+                    activityMemoAddBinding.addMenoSubject.requestFocus();
+
+                    // 키보드를 올려준다.
+                    InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+                    inputMethodManager.showSoftInput(activityMemoAddBinding.addMenoSubject, InputMethodManager.SHOW_FORCED);
+                }
+            });
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.add_menu, menu);
+        return true;
     }
 
     @Override
@@ -45,6 +94,33 @@ public class MemoAddActivity extends AppCompatActivity {
 
         // Home 버튼
         if(itemId == android.R.id.home) {
+            // 현재 액티비티를 종료한다.
+            finish();
+        } else if (itemId == R.id.add_menu_save) { // 저장 버튼
+            // 사용자가 입력한 내용을 가지고 온다.
+            String memoSubject = activityMemoAddBinding.addMenoSubject.getText().toString();
+            String memoText = activityMemoAddBinding.addMenoText.getText().toString();
+
+            // 쿼리문
+            String sql = "insert into MemoTable"
+                       + "(memo_subject, memo_text, memo_data) "
+                       + "values (?, ?, ?)";
+
+            // 현재 시간을 구한다.
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+            String now = simpleDateFormat.format(new Date());
+
+            // 데이터 베이스 오픈
+            DBHelper dbHelper = new DBHelper(this);
+
+            // 저장할 값 배열
+            Object [] arg1 = {memoSubject, memoText, now};
+
+            // 저장한다.
+            SQLiteDatabase sqLiteDatabase = dbHelper.getWritableDatabase();
+            sqLiteDatabase.execSQL(sql, arg1);
+            sqLiteDatabase.close();
+
             // 현재 액티비티를 종료한다.
             finish();
         }
