ce29fa46859d2f12830065a310658b9c3726f863
Hwangtaehun <r283@naver.com>
2026-02-28 20:53:16 +0900

    프로젝트 생성 및 Splash 화면 구현

---
+++ App2LBS/app/src/main/java/com/example/app2lbs/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.app2lbs;
+
+import android.os.Bundle;
+import android.os.SystemClock;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.splashscreen.SplashScreen;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.app2lbs.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding activityMainBinding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+
+        SplashScreen.installSplashScreen(this);
+
+        // SystemClock.sleep(5000);
+
+        activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(activityMainBinding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
