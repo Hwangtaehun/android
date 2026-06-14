f8edbf68694e8bdf9a89696147bfb2d429161b04
Hwangtaehun <r283@naver.com>
2026-06-14 16:41:04 +0900

    AppBar Layout 완성

--- AppBarLayout/app/src/main/java/com/example/appbarlayout/MainActivity.java
+++ AppBarLayout/app/src/main/java/com/example/appbarlayout/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.appbarlayout;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,10 +21,21 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(activityMainBinding.getRoot());
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        setSupportActionBar(activityMainBinding.toolbar);
+        setTitle("타이틀 입니다.");
+
+        // 접혔을 때의 타이틀 색상
+        activityMainBinding.toolbarLayout.setCollapsedTitleTextColor(Color.WHITE);
+        // 펼쳐졌을 때의 타이틀 색상
+        activityMainBinding.toolbarLayout.setExpandedTitleColor(Color.GREEN);
+
+        // 접혔을 때의 타이틀 위치
+        activityMainBinding.toolbarLayout.setCollapsedTitleGravity(Gravity.CENTER_HORIZONTAL);
+        // 펄져졌을 때의 타이틀 위치
+        activityMainBinding.toolbarLayout.setExpandedTitleGravity(Gravity.RIGHT + Gravity.TOP);
+
+        // 펄쳐졌을 때의 이미지 설정
+        // activityMainBinding.appBarImage.setImageResource(R.mipmap.ic_launcher);
     }
 }
